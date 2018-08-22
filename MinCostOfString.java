04871594f89f0231711f0ed8cb4e488c25ea1a03
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 21:06:35 +0530

    Create MinCostOfString.java

---
+++ MinCostOfString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);		
+		String s1=in.next();
+		String s2=in.next();
+		int r=Math.abs(s1.compareTo(s2));
+		System.out.println(r);
+}}
